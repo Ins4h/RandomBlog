4675f6a5158b8cd0ba4327729bad0f5abdb8d94b
Ins <insayy@gmail.com>
2021-07-08 12:52:15 +0200

    add quizz collection

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,7 +5,7 @@ exports.createPages = async ({ graphql, actions }) => {
   const blogTemplate = path.resolve(`src/templates/BlogTemplate.js`)
   const result = await graphql(`
     query MyQuery {
-      allMarkdownRemark {
+      allMarkdownRemark(filter: { frontmatter: { id: { eq: "blog" } } }) {
         edges {
           node {
             frontmatter {
--- src/data/blog/potezny-tytul.md
+++ src/data/blog/potezny-tytul.md
@@ -1,5 +1,5 @@
 ---
-id: blogs
+id: blog
 path: potezny-tytul
 date: 2021-07-06T18:01:56.976Z
 title: Potezniejszy tytul
